645b0b6458c009ff0caa8b8d19486cd3b3e36cd1
Gabriel Guimarães <bielzinhogp@users.noreply.github.com>
2017-12-28 14:39:00 -0200

    Update setup.py

--- ProjetoPAF-2.05/setup.py
+++ ProjetoPAF-2.05/setup.py
@@ -3,8 +3,7 @@ from cx_Freeze import setup, Executable
 setup(
     name="GerarXML",
     version = "1.0.0",
-    author="LINX S.A",
-    author_email="gabriel.pires@linx.com.br / ygohr.campos@linx.com.br",
+    author="Gabriel Guimarães / Ygohr Medeiros",
     description = ".Projeto Gerar XML Estoque e ReducaoZ",
     executables = [Executable("GerarXML.py")])
 
